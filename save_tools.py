44965f50a4b4a5f25fe2ed169eec3f649b8926b4
abfalex <87718997+abfalex@users.noreply.github.com>
2024-04-19 19:14:47 +0300

    Improved readability (recommit)
    - Renamed function names
    - Matched variable names with numbers
    - Improved exception handler

--- save_tools.py
+++ save_tools.py
@@ -3,36 +3,52 @@ import logging
 import random
 import os
 
-
-def download_file(url, folder_path, file_path):
-    response = requests.get(url)
-    response.raise_for_status()
-
-    os.makedirs(folder_path, exist_ok=True)
-
-    with open(f"{folder_path}/{file_path}", 'wb') as file:
-        file.write(response.content)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def download_comics():
-    comics_number = random.randint(1, 2917)
-    comics_title = f"comics_{comics_number}.png"
+def download_and_save_file(url, folder_path, file_name):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        with open(os.path.join(folder_path, file_name), 'wb') as file:
+            file.write(response.content)
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Failed to download and save file from {url}: {e}", exc_info=True)
 
-    url = f"https://xkcd.com/{comics_number}/info.0.json"
 
-    response = requests.get(url)
-    response.raise_for_status()
+def get_comic_info(comic_number):
+    comic_url = f"https://xkcd.com/{comic_number}/info.0.json"
+    try:
+        response = requests.get(comic_url)
+        response.raise_for_status()
+        comic = response.json()
 
-    if not response.ok:
-        return logging.error(f'Ошибка при обработке запроса {url}', exc_info=True)
+        comic_image_url = comic.get("img", "")
+        comic_comment = comic.get("alt", "")
 
-    comics_image_url = response.json().get("img", "")
-    comics_comment = response.json().get("alt", "")
+        return comic_image_url, comic_comment
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Failed to fetch comic info from {comic_url}: {e}", exc_info=True)
+        return None, None
 
-    download_file(
-        url=comics_image_url,
-        folder_path="Files",
-        file_path=comics_title
-    )
 
-    return comics_comment
+def save_comic():
+    try:
+        number_first_comic = 1
+        latest_comic_number = 2917
+
+        comic_number = random.randint(number_first_comic, latest_comic_number)
+        comic_title = f"comic_{comic_number}.png"
+
+        comic_image_url, comic_comment = get_comic_info(comic_number)
+
+        if comic_image_url:
+            download_and_save_file(
+                url=comic_image_url,
+                folder_path="Files",
+                file_name=comic_title
+            )
+            return comic_comment
+    except requests.exceptions.RequestException as e:
+        logging.error(f"An error occurred while downloading comic: {e}", exc_info=True)
+        return None
