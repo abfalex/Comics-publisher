b343b1ae7c0d1d26b8706dca25393c402c9e104f
abfalex <87718997+abfalex@users.noreply.github.com>
2024-04-19 20:10:12 +0300

    Improved the code structure
    - Fixed exceptions
    - Updated environments
    - Updated names variables

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ import os
 import asyncio
 import argparse
 
-from save_tools import download_comics
+from save_tools import save_comic
+from aiogram.exceptions import TelegramAPIError
 from aiogram import Dispatcher, Bot, types
 from dotenv import load_dotenv
 
@@ -13,8 +14,8 @@ async def main():
 
     logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s")
 
-    tg_bot_token = os.getenv('TG_BOT_TOKEN')
-    tg_chat_id = os.getenv('TG_CHAT_ID')
+    tg_bot_token = os.environ['TG_BOT_TOKEN']
+    tg_chat_id = os.environ['TG_CHAT_ID']
 
     bot = Bot(token=tg_bot_token)
     dp = Dispatcher()
@@ -26,30 +27,24 @@ async def main():
                         help='Путь к директории с комиксом')
 
     args = parser.parse_args()
-    comics_directory = args.folder
+    comic_directory = args.folder
 
-    comics_comment = download_comics()
-    comics_files = os.listdir(comics_directory)
+    os.makedirs(comic_directory, exist_ok=True)
 
-    first_file_path = os.path.join(comics_directory, comics_files[0])
-    last_file_path = os.path.join(comics_directory, comics_files[-1])
+    comic_comment = save_comic(comic_directory)
+    comic_files = os.listdir(comic_directory)
 
-    if not comics_files:
-        logging.info(f'Папка {comics_directory} пуста.')
-        return
-
-    if len(comics_files) > 1:
-        os.remove(last_file_path)
+    first_file_path = os.path.join(comic_directory, comic_files[0])
 
     try:
         with open(first_file_path, 'rb'):
             input_file = types.FSInputFile(first_file_path)
-            await bot.send_photo(chat_id=tg_chat_id, photo=input_file, caption=comics_comment)
-            logging.info(f'Отправка фотографии: {first_file_path}')
-
+            await bot.send_photo(chat_id=tg_chat_id, photo=input_file, caption=comic_comment)
+            logging.info(f'Submitting a comic: {first_file_path}')
+    except TelegramAPIError as e:
+        logging.error(f"Error sending comic {first_file_path}: {e}")
+    finally:
         os.remove(first_file_path)
-    except Exception as e:
-        logging.error(f"Ошибка при отправке фотографии {first_file_path}: {e}")
 
     await dp.start_polling(bot)
 
